c7f4e53c2b986a69f454e038fc31c3f48f353462
unknown <M Ahmad Maqbool>
2022-02-10 23:59:26 +0500

    all.c file added

---
+++ all.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+//	a simple cheatsheet of c
+
+	/* simple hello world
+    printf("Hello World!\n");
+    */
+/*	drawing a triangle
+	printf("   /|\n");
+	printf("  / |\n");
+	printf(" /  |\n");
+	printf("/___|\n");
+	*/
+    return 0;
+}
